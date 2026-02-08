c27675e8319aebedc414e8a5da3639e22d4c8e45
bpetersen <bpetersen@ezenki.com>
2026-02-07 18:03:23 -0600

    Setup base

--- postgres-interface/vite.config.ts
+++ postgres-interface/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: "./postcss.config.cjs",
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
-  server: {
-    allowedHosts: true,
-  }
 })
