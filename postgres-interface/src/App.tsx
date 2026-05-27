2ac0e7bc7e5a45334842559b0c06ba1de97b8b61
bpetersen <bpetersen@ezenki.com>
2026-05-26 19:23:04 -0500

    Impelemented base functionality

--- postgres-interface/src/App.tsx
+++ postgres-interface/src/App.tsx
@@ -1,9 +1,59 @@
+import { useRef } from 'react';
 import './App.css'
+import { createWire, RPC, type RPCResponse, type WireSend } from "@ezenki/deploy-commander-installer-interface";
+
+function setupWire(sendAction: (wire: WireSend) => Promise<RPCResponse>){
+  const wire = createWire(sendAction);
+  return RPC.SetupRPCCaller(wire);
+}
 
 export default function App() {
+  const caller = useRef(setupWire(async() => {
+    return {
+      ok: true,
+      result: null
+    }
+  }));
+
   return (
     <div className="p-6 text-xl font-semibold">
-      Postgres Installer Interface
+      <button onClick={() => {
+        const user = "test_user";
+        const password = "strongpassword";
+        caller.current.start(
+          "create", 
+          "ezenki/deploy-commander-runner:latest",
+          {
+            services: {
+              postgres: {
+                image: "postgres:15",
+                environment: {
+                  "POSTGRES_USER": user,
+                  "POSTGRES_PASSWORD": password
+                },
+                resources: [
+                  {
+                    "resource_type": "postgres",
+                    "name": "postgres",
+                    "metadata": {
+                      user,
+                      password
+                    }
+                  }
+                ]
+              }
+            },
+            volumes: [
+              "postgres-data"
+            ]
+          }
+        ).then(() => {
+          console.log("started")
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      }}>Start</button>
     </div>
   )
 }
--- postgres-interface/src/main.tsx
+++ postgres-interface/src/main.tsx
@@ -3,16 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-console.log("Sent");
-    window.parent.postMessage({
-      type: "rpc.call",
-      wireId: "test",
-      request: "run",
-      payload: {
-        check: "Yep"
-      }
-    }, "*")
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
